3a96ac56a1aaa75a68d4c25996f0aed86c802d9b
founder <admin@modestack.co>
2017-11-27 00:20:15 -0500

    added hamburger menu

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './nav.module.scss';
+import MenuIcon from '../../components/icons/menu-icon';
 
 const Nav = () => (
     <nav className={styles.nav}>
@@ -9,6 +10,10 @@ const Nav = () => (
             <li>Blog</li>
             <li>Contact</li>
         </ul>
+        <div className={styles.closeIcon}>
+            <MenuIcon />
+        </div>
+
     </nav>
 );
 
